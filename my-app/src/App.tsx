1e1385491b464fa32eff5bd7781fc2be498636bf
seito-developer <seito.horiguchi@gmail.com>
2021-06-10 17:23:24 +0900

    designed

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,65 +2,63 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const DELAY = 2000;
 const randomCounts = () => {
-  
+  const number1 = Math.round(Math.random() * 100);
+  const number2 = Math.round(Math.random() * 100);
+  const val = Math.floor(number1 * number2);
+
+  return val.toString();
 }
+
+let timer:any;
 
 function App() {
 
   let speech = new SpeechSynthesisUtterance();
-  speech.lang = "en";
-  speech.text = '10000';
   speech.rate = 1;
   speech.volume = 1;
   speech.pitch = 1;
 
-  // window.addEventListener('load', () => {
-  //   console.log(speechSynthesis.getVoices())
-  // })
-  // const synth = window.speechSynthesis;
-  // const voices = synth.getVoices();
-  // console.log(voices);
+  const resetVal = () => {
+    speech.lang = "en";
+    speech.text = randomCounts();
+  }
+  
+  const chaseJp = () => {
+    speech.lang = "ja";
+    window.speechSynthesis.speak(speech);
+  }
 
   const handleClick = () => {
+    resetVal();
     window.speechSynthesis.speak(speech);
     
-    setTimeout(() => {
-      speech.lang = "ja";
-      // let utterance = new SpeechSynthesisUtterance('Ciao')
-      // utterance.lang = 'ja-JP'
-      window.speechSynthesis.speak(speech);
-      // speechSynthesis.onvoiceschanged = () => {
-      //   return 'ja-JP';
-      //   // if(typeof speechSynthesis === 'undefined') {
-      //   //   return;
-      //   // }
-      
-      //   // var voices = speechSynthesis.getVoices();
-      
-      //   // for(var i = 0; i < voices.length; i++) {
-      //   //   var option = document.createElement('option');
-      //   //   option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-      
-      //   //   if(voices[i].default) {
-      //   //     option.textContent += ' -- DEFAULT';
-      //   //   }
-      
-      //   //   option.setAttribute('data-lang', voices[i].lang);
-      //   //   option.setAttribute('data-name', voices[i].name);
-      //   //   document.getElementById("voiceSelect").appendChild(option);
-      //   // }
-      // }
-      
-    }, 2000);
+    clearTimeout(timer);
+    timer = setTimeout(chaseJp, DELAY);
   }
 
   return (
-    <div className="App">
-      {/* <select>
-        { }
-      </select> */}
-      <button type="button" onClick={handleClick}>Click me!</button>
+    <div className="App h-full">
+      <div className="mx-5 mt-20 grid place-content-center">
+        <div className="bg-gradient-to-r from-blue-400 to-indigo-500 rounded-2xl text-white p-8 text-center h-72 mx-auto">
+          <h1 className="text-3xl mb-3 font-bold">Count JP</h1>
+          <p className="text-lg">
+            You can try to recognize numbers from English to Japanese by clicking the button!
+          </p>
+        </div>
+        <div className="bg-white py-8 px-8 text-center rounded-md shadow-lg transform -translate-y-20 sm:-translate-y-24 w-3/4 mx-auto">
+          <h2 className="font-semibold text-2xl mb-6">Count JP</h2>
+          <img className="w-20 h-20 object-cover rounded-full mx-auto shadow-lg" src="https://images.unsplash.com/photo-1611342799915-5dd9f1665d04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="User avatar" />
+          {/* <p className="capitalize text-xl mt-1">essie walton</p> */}
+          <div className="mt-5 w-full">
+            <button 
+              className="rounded-md bg-gradient-to-r from-blue-400 to-indigo-500 text-xl text-white pt-3 pb-4 px-8 inline"
+              type="button" onClick={handleClick}
+            >Listten!</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
