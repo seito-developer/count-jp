6229fcf169498b1e0425a522dc16669c6c81d8cd
seito-developer <seito.horiguchi@gmail.com>
2021-06-10 12:24:32 +0900

    Worked with HTML5 API

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import textToSpeech from '@google-cloud/text-to-speech';
+import fs from 'fs';
+import util from 'util';
+// import { GOOGLE_APPLICATION_CREDENTIALS } from './const';
+
+interface RequestProps {
+  input: any
+  voice: any
+  audioConfig: any
+}
 
 function App() {
+
+  let speech = new SpeechSynthesisUtterance();
+  speech.lang = "en";
+  speech.text = '10000';
+  speech.rate = 1;
+  speech.volume = 1;
+  speech.pitch = 1;
+  let voices = [];
+
+  
+  
+  // const client = new textToSpeech.TextToSpeechClient();
+  
+  // async function quickStart() {
+  //   // The text to synthesize
+  //   const text = 'hello, world!';
+
+  //   // Construct the request
+  //   const request:RequestProps = {
+  //     input: {text: text},
+  //     // Select the language and SSML voice gender (optional)
+  //     voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+  //     // select the type of audio encoding
+  //     audioConfig: {audioEncoding: 'MP3'}
+  //   };
+
+  //   // Performs the text-to-speech request
+  //   const [response] = await client.synthesizeSpeech(request);
+  //   // Write the binary audio content to a local file
+  //   const writeFile = util.promisify(fs.writeFile);
+  //   await writeFile('output.mp3', response.audioContent, 'binary');
+  //   console.log('Audio content written to file: output.mp3');
+  // }
+
+  const handleClick = () => {
+    window.speechSynthesis.speak(speech);
+    // quickStart();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={handleClick}>Click me!</button>
     </div>
   );
 }
